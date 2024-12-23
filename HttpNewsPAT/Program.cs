05f7dba8041796271a4ebb4ffcecbd960e4d145a
twozziey <twozziey@mail.ru>
2024-12-23 10:41:09 +0500

    Получение информации с сайта. Осокин

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpNewsPAT
 {
-    internal class Program
+    public class Program
     {
+        private static HttpClient _httpClient;
+        private static string _cookie;
+
         static void Main(string[] args)
         {
-
+            WebRequest request = WebRequest.Create(" http://news.permaviat.ru/main ");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
